0d45010129da770185f489ad85278b8e8dd5db3d
Elvis Nuñez <elvisnunez@me.com>
2015-06-20 09:42:50 +0200

    Add simple form

--- Tests/App/AppDelegate.swift
+++ Tests/App/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import JSON
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,8 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        FORMDefaultStyle.applyStyle()
+
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        self.window!.backgroundColor = UIColor.whiteColor()
+
+        let (json: AnyObject?, error) = JSON.from("forms.json")
+        self.window?.rootViewController = SampleController(JSON: json!, andInitialValues: nil, disabled: false)
         self.window!.makeKeyAndVisible()
         return true
     }
--- Tests/App/SampleController.swift
+++ Tests/App/SampleController.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class SampleController: FORMViewController {
+    override init(JSON: AnyObject, andInitialValues initialValues: [NSObject : AnyObject]?, disabled: Bool) {
+        super.init(JSON: JSON, andInitialValues: initialValues, disabled: disabled)
+    }
 
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
