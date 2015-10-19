faea058185e0577cdf09973563ef12f4b2be491f
Benjamin Uzelac <ben@buzelac.com>
2015-10-19 14:12:22 -0500

    allow custom heights for all fields

--- Source/FORMDataSource.m
+++ Source/FORMDataSource.m
@@ -306,7 +306,7 @@ static const CGFloat FORMKeyboardAnimationDuration = 0.3f;
     } else if (field) {
         width = floor(deviceWidth * (field.size.width / 100.0f));
 
-        if (field.type == FORMFieldTypeCustom) {
+        if (field.type == FORMFieldTypeCustom || field.size.height > 0) {
             height = field.size.height * FORMFieldCellItemHeight;
         } else {
             height = FORMFieldCellItemHeight;
